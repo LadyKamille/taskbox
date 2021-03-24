a23eb6c3286bdd899e0991d06dc7f2cb29562289
Kamille Norris <kamionorato@gmail.com>
2021-03-24 15:30:41 -0400

    Updates

---
+++ precache-manifest.13c11bd4b36a51f00aa4c4844d11a57d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b232759646128822ff2fa6123ba8b9aa",
+    "url": "/taskbox/index.html"
+  },
+  {
+    "revision": "8d1679e240a7089299bb",
+    "url": "/taskbox/static/css/main.f0d389a0.chunk.css"
+  },
+  {
+    "revision": "751afd307c769e01bd12",
+    "url": "/taskbox/static/js/2.a5f34051.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/taskbox/static/js/2.a5f34051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1cce35dc60869ec3f5",
+    "url": "/taskbox/static/js/3.1f174972.chunk.js"
+  },
+  {
+    "revision": "8d1679e240a7089299bb",
+    "url": "/taskbox/static/js/main.7a83a8d4.chunk.js"
+  },
+  {
+    "revision": "f5ff7ef072390c768b3f",
+    "url": "/taskbox/static/js/runtime-main.422629dc.js"
+  }
+]);
